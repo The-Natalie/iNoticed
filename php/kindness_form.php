8f6dbca96f5d1b25d4e0a7ea24c892b9eb267024
Natalie Hall <natalie.diana.hall@gmail.com>
2019-10-17 16:30:45 -0400

    msql test

--- php/kindness_form.php
+++ php/kindness_form.php
@@ -6,9 +6,10 @@ mysql_select_db("inoticed_kindness_cards_requests", $connection);
 $name2=$_POST['name1']; 
 $email2=$_POST['email1'];
 $cardsRequested2=$_POST['cardsRequested1'];
-$query = mysql_query("INSERT INTO kindness_cards (name, email, cardsRequested) VALUES ('$name2','$email2','$cardsRequested2')"); //Insert query
+$query = mysql_query("INSERT INTO kindness_cards VALUES ($name2, $email2, $cardsRequested2)"); //Insert query
 if($query){
 echo "Data Submitted succesfully";
 }
 mysql_close($connection); // Connection Closed.
 ?>
+

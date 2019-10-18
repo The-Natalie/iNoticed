7a994d95d365f9472f8fb0acae9703d972057132
Natalie Hall <natalie.diana.hall@gmail.com>
2019-10-17 22:16:14 -0400

    msyql test

--- php/kindness_form.php
+++ php/kindness_form.php
@@ -12,21 +12,21 @@ $dbname = "inoticed_kindness_cards_requests";
 // Create connection
 $conn = new mysqli ($host, $dbusername, $dbpassword, $dbname);
 if (mysqli_connect_error()){
-die('Connect Error ('. mysqli_connect_errno() .') '
-. mysqli_connect_error());
+	die('Connect Error ('. mysqli_connect_errno() .') '
+	. mysqli_connect_error());
 }
-else{
-$sql = "INSERT INTO kindness_cards (name, email, cardsRequested)
-values ('$name','$email', '$cardsRequested')";
-if ($conn->query($sql)){
-echo "New record is inserted sucessfully";
-}
-else{
-echo "Error: ". $sql ."
-". $conn->error;
-}
-$conn->close();
-}
+	else{
+		$sql = "INSERT INTO kindness_cards (name, email, cardsRequested)
+		values ('$name','$email', '$cardsRequested')";
+		if ($conn->query($sql)){
+			echo "New record is inserted sucessfully";
+		}
+		else{
+			echo "Error: ". $sql ."
+			". $conn->error;
+		}
+		$conn->close();
+	}
 }
 else{
 echo "Name should not be empty";
@@ -37,5 +37,7 @@ else{
 echo "Email should not be empty";
 die();
 }
+}
 ?>
+
 

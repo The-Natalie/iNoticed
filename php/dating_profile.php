4a8108cec1bc0cd4e655f033edf4ffa049258bd8
Natalie Hall <natalie.diana.hall@gmail.com>
2019-10-21 17:05:37 -0400

    add logout and profile phps

--- php/dating_profile.php
+++ php/dating_profile.php
@@ -30,7 +30,7 @@ $stmt->close();
 	<head>
 		<meta charset="utf-8">
 		<title>Profile Page</title>
-		<link href="style.css" rel="stylesheet" type="text/css">
+		<link rel="stylesheet" type="text/css" href="/css/styles.css"> 
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
 	</head>
 	<body class="loggedin">

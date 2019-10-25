1a158f377dc49160551edc16e00b803b85e4f8be
Natalie Hall <natalie.diana.hall@gmail.com>
2019-10-24 21:51:57 -0400

    update test

--- php/update.php
+++ php/update.php
@@ -9,8 +9,9 @@ $DATABASE_NAME = 'inoticed_dating';
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
 if ( mysqli_connect_errno() ) {
     // If there is an error with the connection, stop the script and display the error.
-    die (header('Location: /php/account_settings.php');
-        $php_results = 'Failed to connect to MySQL: ' . mysqli_connect_error());
+    header('Location: /php/account_settings.php');
+    $php_results = 'Failed to connect to MySQL: ' . mysqli_connect_error();
+    exit();
 }
 
 $stmt = $con->prepare('SELECT password, email, id FROM accounts WHERE id = ?');

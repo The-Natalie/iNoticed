a135f8207f5bb6d011c423790a2d73e21f89b2b3
Natalie Hall <natalie.diana.hall@gmail.com>
2019-10-25 01:41:06 -0400

    update test

--- php/update_email.php
+++ php/update_email.php
@@ -135,33 +135,31 @@ mysqli_stmt_close($stmt);
 mysqli_close($con);
 ?>
 
-
 <!DOCTYPE html>
-<html>
+<html lang="en">
   <head>
-    <meta charset="utf-8">
     <title>iNoticed | Dating</title>
+    <meta charset="utf-8">  
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="/css/styles.css"> 
-    <link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet"> 
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css"> 
+    <link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet">  
   </head>
-  <body class="loggedin">
+  <body id="loggedin">
 
-    <div class="nav-light">
-      <div class="nav-left">
-        <div class="title"><a href="/">iNoticed</a></div>
+      <div class="nav-light">
+        <div class="nav-left">
+          <div class="title"><a href="/">iNoticed</a></div>
+        </div>
+        <div class="nav-right">
+          <a href="/php/dating_home.php"><i class="fas fa-envelope"></i>Home</a>
+          <a href="/php/messages.php"><i class="fas fa-envelope"></i>Messages</a>
+          <a href="/php/dating_profile.php"><i class="fas fa-address-card"></i>My Profile</a>
+          <a href="/php/account_settings.php"><i class="fas fa-cog"></i>Account Settings</a>
+          <a href="/php/dating_logout.php"><i class="fas fa-sign-out-alt"></i>Log Out</a>
+        </div>
       </div>
-      <div class="nav-right">
-        <a href="/php/dating_home.php"><i class="fas fa-envelope"></i>Home</a>
-        <a href="/php/messages.php"><i class="fas fa-envelope"></i>Messages</a>
-        <a href="/php/dating_profile.php"><i class="fas fa-address-card"></i>My Profile</a>
-        <a href="/php/account_settings.php"><i class="fas fa-cog"></i>Account Settings</a>
-        <a href="/php/dating_logout.php"><i class="fas fa-sign-out-alt"></i>Log Out</a>
-      </div>
-    </div>
 
-    <div class="content">
+      <div class="content">
       <h2>Home Page</h2>
         <p><?php echo $php_results; ?></p>            
     </div>
@@ -176,4 +174,3 @@ mysqli_close($con);
     <script type="text/javascript" src="/js/scripts.js"></script>
   </body>
 </html>
- 

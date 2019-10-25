5a6348e603d1b3322b8d0f5d8e2452a44754c1fd
Natalie Hall <natalie.diana.hall@gmail.com>
2019-10-25 02:14:11 -0400

    update test

--- php/update_email1.php
+++ php/update_email1.php
@@ -44,7 +44,7 @@ if(isset($_POST["id"]) && !empty($_POST["id"])){
             // Attempt to execute the prepared statement
             if(mysqli_stmt_execute($stmt)){
                 // Records updated successfully. Redirect to landing page
-                header("location: inoticed.org");
+                echo "Records updated successfully";        
                 exit();
             } else{
                 echo "Something went wrong. Please try again later.";

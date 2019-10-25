7a5f094e3306ce9fcbf09a8ee444207889457745
Natalie Hall <natalie.diana.hall@gmail.com>
2019-10-24 21:12:04 -0400

    update test

--- php/update.php
+++ php/update.php
@@ -63,11 +63,11 @@ if(isset($_POST["id"]) && !empty($_POST["id"])){
         // Check input errors before inserting in database
         if(empty($php_results)){
             // Prepare an update statement
-            $sql = "UPDATE accounts SET email=?, password=? WHERE id=?";
+            $sql = "UPDATE accounts SET email=?, password=? WHERE id=$id;"
              
             if($stmt = mysqli_prepare($con, $sql)){
                 // Bind variables to the prepared statement as parameters
-                mysqli_stmt_bind_param($stmt, "sssi", $param_email, $param_password, $param_id);
+                mysqli_stmt_bind_param($stmt, "ssi", $param_email, $param_password, $param_id);
                 
                 // Set parameters
                 $param_email = $email;

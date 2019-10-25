c6521292bbe0ac2743ad5d66b9a015f01d7543ea
Natalie Hall <natalie.diana.hall@gmail.com>
2019-10-25 01:58:07 -0400

    update test

--- php/update_email.php
+++ php/update_email.php
@@ -9,8 +9,7 @@ $DATABASE_NAME = 'inoticed_dating';
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
 if ( mysqli_connect_errno() ) {
     // If there is an error with the connection, stop the script and display the error.
-    $php_results = 'Failed to connect to MySQL: ' . mysqli_connect_error();
-    exit();
+    die ('Failed to connect to MySQL: ' . mysqli_connect_error());
 }
 
 $stmt = $con->prepare('SELECT email, id FROM accounts WHERE id = ?');

ca01547c6a929201b6ea5dff9697b4f2ab70d882
Natalie Hall <natalie.diana.hall@gmail.com>
2019-10-29 23:43:57 -0400

    two tables created at register

--- php/register.php
+++ php/register.php
@@ -77,7 +77,7 @@ $stmt->store_result();
 $stmt->bind_result($id);
 $stmt->fetch();
 
-$sql = "INSERT INTO profiles (id) VALUES ($id)";
+$sql = "INSERT INTO profiles (id) VALUES ($_SESSION['id'])";
 if ($con->query($sql) === TRUE) {
     echo "New record created successfully";
 } else {

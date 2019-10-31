ce529f9eb28c96800a907e73db62e490044f533b
Natalie Hall <natalie.diana.hall@gmail.com>
2019-10-30 20:28:29 -0400

    edit profile form completed

--- php/profile.php
+++ php/profile.php
@@ -17,11 +17,11 @@ if (mysqli_connect_errno()) {
 	die ('Failed to connect to MySQL: ' . mysqli_connect_error());
 }
 // We don't have the password or email info stored in sessions so instead we can get the results from the database.
-$stmt = $con->prepare('SELECT password, email FROM accounts WHERE id = ?');
+$stmt = $con->prepare('SELECT email FROM accounts WHERE id = ?');
 // In this case we can use the account ID to get the account info.
 $stmt->bind_param('i', $_SESSION['id']);
 $stmt->execute();
-$stmt->bind_result($password, $email);
+$stmt->bind_result($email);
 $stmt->fetch();
 $stmt->close();
 ?>
@@ -51,33 +51,150 @@ $stmt->close();
 				<button class="edit-profile-button" type="button">Edit profile  <i class="far fa-edit"></i></button>
 				<div id="edit-profile-form">
 					<form method="post" action="/php/edit_profile.php">
-<!-- 						<p style="font-size: 0.8em;">Your current password is required to change information.</p  -->
- 								<input type="email" name="email" placeholder="New Email"><br />
-<!-- 						<input id="password" name="password" type="password" placeholder="Current Password" ><br /-->
-						<input type="hidden" name="id" value="<?php echo $id; ?>"/>
+ 						<p>First Name:<input type="text" name="fname" value="<?php echo $fname; ?>"></p>
+ 						<p>Age:<input type="number" name="age" value="<?php echo $age; ?>"></p>
+ 						<p>Gender:
+	 						<select name="gender" size="1" value="<?php echo $gender; ?>">
+						    <option value="female">Female</option>
+						    <option value="male">Male</option>
+						    <option value="queer">Queer</option>
+						    <option value="nonbinary">Nonbinary</option>
+						    <option value="genderfluid">Genderfluid</option>
+						    <option value="agender">Agender/Gender-Neutral</option>
+						    <option value="transgender">Transgender</option>
+						    <option value="gender-trans">Gender Transition</option>
+						  </select>
+						</p>
+ 						<p>Height:
+ 							<select name="fheight" size="1" value="<?php echo $fheight; ?>">
+						    <option value="f2">2</option>
+						    <option value="f3">3</option>
+						    <option value="f4">4</option>
+						    <option value="f5">5</option>
+						    <option value="f6">6</option>
+						    <option value="f7">7</option>Feet
+					  	</select>
+					  	<select name="iheight" size="1" value="<?php echo $iheight; ?>">
+					  		<option value="i0">0</option>
+					  		<option value="i1">1</option>
+						    <option value="i2">2</option>
+						    <option value="i3">3</option>
+						    <option value="i4">4</option>
+						    <option value="i5">5</option>
+						    <option value="i6">6</option>
+						    <option value="i7">7</option>
+						    <option value="i8">8</option>
+						    <option value="i9">9</option>
+						    <option value="i10">10</option>
+						    <option value="i11">11</option>
+						    <option value="i12">12</option>Inches
+					  	</select>
+					  </p>
+				  	<p>Eye Color:
+							<select name="eyes" size="1" value="<?php echo $eyes; ?>">
+						    <option value="blue">Blue</option>
+						    <option value="green">Green</option>
+						    <option value="brown">Brown</option>
+						    <option value="hazel">Hazel</option>
+						    <option value="grey">Grey</option>
+						    <option value="blue-green">Blue & Green</option>
+						    <option value="fake-color">Colored Contacts</option>
+				  		</select>
+				  	</p>
+				  	<p>Hair Color:
+							<select name="hair" size="1" value="<?php echo $hair; ?>">
+						    <option value="blond">Blond</option>
+						    <option value="brown">Brown</option>
+						    <option value="red">Red</option>
+						    <option value="black">Black</option>
+						    <option value="white">White</option>
+						    <option value="grey">Grey</option>
+						    <option value="fake-color">Unnatural Color</option>
+				  		</select>
+				  	</p>
+				  	<p>What kind of relationship I'm looking for:
+							<select name="intention" size="1" value="<?php echo $intention; ?>">
+						    <option value="serious">Serious/Long Term</option>
+						    <option value="dating">Dating/Short Term</option>
+						    <option value="marriage">Marriage</option>
+						    <option value="looking">Just Looking</option>
+						    <option value="hookup">Hookup</option>
+						    <option value="fwb">Friends With Benefits</option>
+				  		</select>
+				  	</p>
+ 						<p>About me and what I'm looking for:<input type="text" name="about-me" value="<?php echo $aboutMe; ?>"></p>
+						<p>Location (enter your zip and the rest will be completed automatically):
+							<fieldset>
+								<div id="zipbox" class="control-group">
+									<label for="zip">Zip</label>
+									<input type="text" class=” pattern="[0-9]*" name="zip" id="zip" value="<?php echo $zip; ?>"/>
+								</div>
+								<div>
+									<div id="citybox" class="control-group">
+										<label for="city">City</label>
+										<input type="text" name="city" id="city" value="<?php echo $city; ?>">" />
+									</div>
+									<div id="statebox" class="control-group">
+										<label for="state">State</label>
+										<input type="text" name="state" id="state" value="<?php echo $state; ?>"/>
+									</div>
+								</div>
+							</fieldset></p>
+ 						<p>Profession:<input type="text" name="profession" value="<?php echo $profession; ?>"></p>
+ 						<p>Highest level of education:
+ 							<select name="education" size="1" value="<?php echo $education; ?>">
+						    <option value="pre-highS">Some High School or Lower</option>
+						    <option value="highS">High School Degree/GED</option>
+						    <option value="pre-coll">Some College</option>
+						    <option value="associate">Associate's Degree</option>
+						    <option value="bachelor">Bachelor's Degree</option>
+						    <option value="master">Master's Degree</option>
+						    <option value="doctor">Doctoral Degree</option>
+				  		</select>
+				  	</p>
+				  	<p>Ethnicity:
+				  		<select name="ethnicity" size="1" value="<?php echo $ethnicity; ?>">
+						    <option value="asian">Asian</option>
+						    <option value="black">Black/African</option>
+						    <option value="white">Caucasian</option>
+						    <option value="hispanic">Hispanic/Latino</option>
+						    <option value="native">Native American</option>
+						    <option value="pacific">Pacific Islander</option>
+						    <option value="mixed">Mixed Race</option>
+						    <option value="other">Other</option>
+				  		</select>
+				  	</p>
+ 						<p>Religion<input type="text" name="religion" value="<?php echo $religion; ?>"></p>
+ 						<p>Marital status:
+				  		<select name="marital" size="1" value="<?php echo $marital; ?>">
+						    <option value="single">Single</option>
+						    <option value="relationship">In a Relationship</option>
+						    <option value="engaged">Engaged</option>
+						    <option value="married">Married</option>
+						    <option value="divorced">Divorced</option>
+						    <option value="seperated">Seperated</option>
+						    <option value="widowed">Widowed</option>
+				  		</select>
+				  	</p>
+				  	<p>Have kids (if yes, how many)?:
+				  		<select name="kids" size="1" value="<?php echo $kids; ?>">
+						    <option value="yes1-2">Yes - 1 or 2</option>
+						    <option value="yes3-4">Yes - 3 or 4</option>
+						    <option value="yes5-6">Yes - 5 or 6</option>
+						    <option value="yes7-plus">Yes - 7+</option>
+						    <option value="no">No</option>
+				  		</select>
+				  	</p>
+				  	<p>Want kids?:
+				  		<select name="want-kids" size="1" value="<?php echo $wantKids; ?>">
+						    <option value="yes">Yes</option>
+						    <option value="no">No</option>
+				  		</select>
+				  	</p>
+ 						<input type="hidden" name="id" value="<?php echo $id; ?>"/>
  						<input style="margin-top: 10px;" type="submit" value="Submit">
 					</form>	
 				</div>
-				need to create new table for profile info that shares the same id from accounts table.
-				need to pull info from profile info and displaty it in the forms, so that when they edit their profile they won't have to update every field every time.  
-				first name -text field
-				height -number scrolls
-				age - number scroll
-				gender -radio
-				what I'm looking for/intention (hookup, dating, marriage, etc...) - scroll
-				self summary /about me -text field
-				location - zip code search? or text field?
-				profession -text field
-				education -scroll
-				ethnicity -scroll/radio
-				religion -scroll or text field
-				marital status - scroll
-				have kids -radio
-					if yes, how many - -number scroll
-				want kids - radio
-				hair -scroll
-				eyes -scroll
-				hidden id to use to post to table
 			</div>
 		</div>
 

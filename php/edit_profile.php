8df916f65070cf2bff2201e6088445ff3fd4ecff
Natalie Hall <natalie.diana.hall@gmail.com>
2019-11-08 17:11:13 -0500

    finish profile edit pages including php- test

--- php/edit_profile.php
+++ php/edit_profile.php
@@ -27,6 +27,7 @@ $stmt->fetch();
 $stmt->close();
 ?>
 
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <script type="text/javascript">$(document).ready(function(){
 
 	var gender = "<?=$gender?>";   

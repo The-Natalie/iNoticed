de814d027829d7e40d50ff34d19579c9eba3e87f
Natalie Hall <natalie.diana.hall@gmail.com>
2019-11-09 14:18:11 -0500

    finish profile edit pages including php- test

--- php/updated_profile.php
+++ php/updated_profile.php
@@ -45,7 +45,7 @@ if(isset($_POST["id"]) && !empty($_POST["id"])){
              
             if($stmt = mysqli_prepare($con, $sql)){
                 // Bind variables to the prepared statement as parameters
-                mysqli_stmt_bind_param($stmt, "siis", $param_first_name, $param_age, $param_gender, $param_id);
+                mysqli_stmt_bind_param($stmt, "sisi", $param_first_name, $param_age, $param_gender, $param_id);
                 
                 // Set parameters
                 $param_first_name = $first_name;

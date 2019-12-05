4cda976ee5402ae5c72ab9b9cabe381ae0b165ec
Natalie Hall <natalie.diana.hall@gmail.com>
2019-12-05 16:14:18 -0500

    testing php object in js

--- php/edit_profile.php
+++ php/edit_profile.php
@@ -24,7 +24,7 @@ $stmt->execute();
 $stmt->bind_result($id, $first_name, $age, $gender, $feet, $inches, $eyes, $hair, $smoke, $drugs, $transportation, $intention, $zip, $city, $state, $profession, $education, $ethnicity, $religion, $marital_status, $kids, $want_kids, $about_me);
 $stmt->fetch();
 
-$education = htmlspecialchars($education);
+$education = JSON.parse($education);
 // $ethnicity = htmlspecialchars($ethnicity);
 // $marital_status = htmlspecialchars($marital_status);
 // $want_kids = htmlspecialchars($want_kids);

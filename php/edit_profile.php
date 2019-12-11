6054a0d47984e4e0362f49da4c551492b64bc669
Natalie Hall <natalie.diana.hall@gmail.com>
2019-12-10 19:40:26 -0500

    image upload test

--- php/edit_profile.php
+++ php/edit_profile.php
@@ -157,7 +157,7 @@ $stmt->close();
 				<p>Upload main profile image:</p>
 				<p>(Max image size is 2MB. Any image added will override the current main image.)</p>
  				<div class="form-container">
-      		<form action="/php/edit_images.php" id="uploadForm" name="frmupload" method="post" enctype="multipart/form-data">
+      		<form  id="uploadForm" name="frmupload" method="post" enctype="multipart/form-data">
           	<input type="file" id="uploadImage" name="image_main" class="main-img" accept="image/*" /> 
           	<input id="submitButton" class="submit-button" type="submit" name='btnSubmit' value="Submit Image" />
       		</form>

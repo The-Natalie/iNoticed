a47761ebede6933a0eb78f10836b4b571c6c5a58
Natalie Hall <natalie.diana.hall@gmail.com>
2019-12-11 19:04:55 -0500

    image upload test

--- php/edit_images.php
+++ php/edit_images.php
@@ -17,7 +17,7 @@ if (!isset($_SESSION['loggedin'])) {
 // }
 
 
-$target_dir = "/php/uploads/";
+$target_dir = "uploads/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));

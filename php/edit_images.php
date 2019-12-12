e114e680696c15e82e1d5561abc2fb56e8d1af1c
Natalie Hall <natalie.diana.hall@gmail.com>
2019-12-11 19:15:54 -0500

    image upload test

--- php/edit_images.php
+++ php/edit_images.php
@@ -26,12 +26,12 @@ $stmt->fetch();
 
 
 $target_dir = "uploads/";
-$target_file = $target_dir . basename($_FILES["fileToUpload"][$username]]["name"]);
+$target_file = $target_dir . basename($_FILES["fileToUpload"][$username]["name"]);
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 // Check if image file is a actual image or fake image
 if(isset($_POST["submit"])) {
-    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+    $check = getimagesize($_FILES["fileToUpload"][$username]["tmp_name"]);
     if($check !== false) {
         echo "File is an image - " . $check["mime"] . ".";
         $uploadOk = 1;
@@ -61,8 +61,8 @@ if ($uploadOk == 0) {
     echo "Sorry, your file was not uploaded.";
 // if everything is ok, try to upload file
 } else {
-    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+    if (move_uploaded_file($_FILES["fileToUpload"][$username]["tmp_name"], $target_file)) {
+        echo "The file ". basename( $_FILES["fileToUpload"][$username]["name"]). " has been uploaded.";
     } else {
         echo "Sorry, there was an error uploading your file.";
     }

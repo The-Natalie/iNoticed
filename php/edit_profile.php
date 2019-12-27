fd7155f7935b127c90cc2060b6222704d6ba2deb
Natalie Hall <natalie.diana.hall@gmail.com>
2019-12-27 13:04:25 -0500

    post image path to mysql

--- php/edit_profile.php
+++ php/edit_profile.php
@@ -159,6 +159,7 @@ $stmt->close();
 				<form action="/php/edit_images.php" method="post" enctype="multipart/form-data">
     Select image to upload:
     <input type="file" name="fileToUpload" id="fileToUpload">
+    <input type="hidden" name="id" value="<?php echo $id; ?>"/>
     <input type="submit" value="Upload Image" name="submit">
 </form>
 

2dd4cf604125619c1f58b1d9adec691d76fd541e
Natalie Hall <natalie.diana.hall@gmail.com>
2019-12-31 15:22:09 -0500

    testing main profile image upload

--- php/edit_images.php
+++ php/edit_images.php
@@ -74,15 +74,17 @@ if ($uploadOk == 0) {
         if(empty($is_error)){
           $value = $_POST['fileToUpload'];
           // Prepare an update statement
-          $sql = "UPDATE accounts SET $value=? WHERE id=?";
+          $sql = "UPDATE accounts SET ?=? WHERE id=?";
                  
           if($stmt = mysqli_prepare($con, $sql)){
             // Bind variables to the prepared statement as parameters
-            mysqli_stmt_bind_param($stmt, "si", $param_target_file, $param_id);
+            mysqli_stmt_bind_param($stmt, "ssi", $param_value, $param_target_file, $param_id);
                     
             // Set parameters
+            $param_value = $value;
             $param_target_file = $target_file;
             $param_id = $id;
+
                     
             // Attempt to execute the prepared statement
             if(mysqli_stmt_execute($stmt)){

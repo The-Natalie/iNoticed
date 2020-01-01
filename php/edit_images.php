19d4451d06de90d0cf6c457116f79af415c60b2a
Natalie Hall <natalie.diana.hall@gmail.com>
2019-12-31 21:30:06 -0500

    testing other image upload

--- php/edit_images.php
+++ php/edit_images.php
@@ -24,144 +24,6 @@ $stmt->store_result();
 $stmt->bind_result($username);
 $stmt->fetch();
 
-// $target_dir = "uploads/";
-// $target_file = $target_dir . $username . "_" . basename($_FILES["fileToUpload"]["name"]);
-// $uploadOk = 1;
-// $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-// $value = $_POST["img_value"]; 
-
-// // Check if image file is a actual image or fake image
-// if(isset($_POST["submit"])) {
-//   $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-//   if($check !== false) {
-//       $uploadOk = 1;
-//   } else {
-//       $param2 = "File is not an image.";
-//       $uploadOk = 0;
-//   }
-// }
-// // Check if file already exists
-// if (file_exists($target_file)) {
-//     $param2 = "Sorry, file already exists.";
-//     $uploadOk = 0;
-// }
-// // Check file size
-// if ($_FILES["fileToUpload"]["size"] > 2097153) {
-//   $param2 = "Sorry, your file is too large.";
-//   $uploadOk = 0;
-// }
-// // Allow certain file formats
-// if($imageFileType != "jpg" && $imageFileType != "JPG" && $imageFileType != "png" && $imageFileType != "PNG" && $imageFileType != "jpeg" && $imageFileType != "JPEG" && $imageFileType != "gif" && $imageFileType != "GIF" ) {
-//   $param2 = "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-//   $uploadOk = 0;
-// }
-// // Check if $uploadOk is set to 0 by an error
-// if ($uploadOk == 0) {
-//   $param1 = "Sorry, your file was not uploaded.";
-// // if everything is ok, try to upload file
-// } else {
-//     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-//       $param1 = "Your photo has been saved as ". basename( $_FILES["fileToUpload"]["name"]). " ";
-    
-//       $is_error = "";
-
-//       // Processing form data when form is submitted
-//       if(isset($_POST["id"]) && !empty($_POST["id"])){
-//         // Get hidden input value
-//         $id = $_POST["id"];
-
-//         // Check input errors before inserting in database
-//         if(empty($is_error)){
-//           // Prepare an update statement
-//           $sql = "UPDATE accounts SET ".$value."=? WHERE id=?";
-
-//           if($stmt = mysqli_prepare($con, $sql)){
-//             // Bind variables to the prepared statement as parameters
-//             mysqli_stmt_bind_param($stmt, "si", $param_target_file, $param_id);
-                    
-//             // Set parameters
-//             $param_target_file = $target_file;
-//             $param_id = $id;
-                    
-//             // Attempt to execute the prepared statement
-//             if(mysqli_stmt_execute($stmt)){
-//               // Records updated successfully. 
-//               $param = "It has been posted sucessfully.";
-//             } else { 
-//               $param = "It was NOT posted sucessfully. Please try again later. Or let dating@inoticed.org know the details of your problem.";
-//                $is_error = "1";
-//                 }
-//             }
-             
-//             // Close statement
-//             mysqli_stmt_close($stmt);
-//         }
-        
-//         // Close connection
-//         mysqli_close($con);
-
-//       } else{
-//         // Check existence of id parameter before processing further
-//         if(isset($_GET["id"])){
-//           // Get URL parameter
-//           $id =  trim($_GET["id"]);
-              
-//           // Prepare a select statement
-//           $sql = "SELECT * FROM accounts WHERE id = ?";
-//           if($stmt = mysqli_prepare($con, $sql)){
-//             // Bind variables to the prepared statement as parameters
-//             mysqli_stmt_bind_param($stmt, "i", $param_id);
-                  
-//             // Set parameters
-//             $param_id = $id;
-                  
-//             // Attempt to execute the prepared statement
-//             if(mysqli_stmt_execute($stmt)){
-//               $result = mysqli_stmt_get_result($stmt);
-          
-//               if(mysqli_num_rows($result) == 1){
-//                 /* Fetch result row as an associative array. Since the result set
-//                 contains only one row, we don't need to use while loop */
-//                 $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
-                  
-//                 // Retrieve individual field value
-//                 $target_file = $row["image_main"];
-//               } else{
-//                 // URL doesn't contain valid id. 
-//                 $param = "Please sign out, sign back in, and try again. Or let dating@inoticed.org know the details of your problem.";                    
-//                 $is_error = "1";
-//                 exit();
-//               }
-
-//             } else{
-//                 $param = "Oops! Something went wrong. Please try again later. Or let dating@inoticed.org know the details of your problem.";
-//                 $is_error = "1";
-//             }
-//           }
-              
-//               // Close statement
-//               mysqli_stmt_close($stmt);
-              
-//               // Close connection
-//               mysqli_close($con);
-//         }  else{
-//           // URL doesn't contain id parameter. 
-//           $param = "URL doesn't contain id parameter. Please sign out, sign back in, and try again. Or let dating@inoticed.org know the details of your problem.";
-//           $is_error = "1";
-//         }
-//       }
-
-//   } else {
-//     $param1 = "Sorry, there was an error uploading your file.";
-//   }
-// }
-
-
-
-
-
-
-
 if (isset($_POST['btnSubmit'])) {
   $uploadfile = $_FILES["uploadImage"]["tmp_name"];
   $folderPath = "uploads/";
@@ -268,7 +130,7 @@ mysqli_close($con);
 
 
 
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html>
   <head>
     <meta charset="utf-8">
@@ -302,4 +164,4 @@ mysqli_close($con);
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <script type="text/javascript" src="/js/scripts.js"></script>
   </body>
-</html>
+</html> -->

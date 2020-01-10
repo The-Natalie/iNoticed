007586b06ac21532ead4ec65debf45e565284712
Natalie Hall <natalie.diana.hall@gmail.com>
2020-01-09 20:33:00 -0500

    testing main image deletion- mysql was successful, but server was not

--- js/scripts.js
+++ js/scripts.js
@@ -398,7 +398,7 @@ $(document).ready(function(){
 	//Delete image
   $('button.delete-main-img').click(function(){ 
     // Selecting image source
-    var imgElement_src = $("img.preview-images.image_main").attr("src");
+    var imgElement_src = $("img.image_main").attr("src");
 
     //Selecting image value and user ID for mysql column
     var imgValue = $('#value-main-image').val();
